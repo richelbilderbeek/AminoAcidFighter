c55d489e7989963e22ca3eb2133f1ad1763e7146
CarmenIJsebaart <cijsebaart@gmail.com>
2017-01-09 18:10:13 +0100

    Glycin moves

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
@@ -10,6 +11,15 @@ int main()
     "AminoAcidFighter",
     sf::Style::Titlebar | sf::Style::Close);
 
+  sf::Texture glycin_texture;
+  glycin_texture.loadFromFile("Glycin.png");
+
+  sf::Sprite glycin_sprite;
+  glycin_sprite.setTexture(glycin_texture);
+  glycin_sprite.setScale(sf::Vector2f(0.1, 0.1));
+  glycin_sprite.setOrigin(sf::Vector2f(443.517, 208.489));
+  glycin_sprite.setPosition(sf::Vector2f(200, 200));
+
   while(window.isOpen())
   {
     sf::Event event;
@@ -21,11 +31,41 @@ int main()
         case sf::Event::Closed:
           window.close();
           break;
+        case sf::Event::KeyPressed:
+        {
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+          {
+            auto glycin_rot = glycin_sprite.getRotation();
+            glycin_rot -= 5;
+            glycin_sprite.setRotation(glycin_rot);
+          }
+          else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+          {
+            auto glycin_rot = glycin_sprite.getRotation();
+            glycin_rot += 5;
+            glycin_sprite.setRotation(glycin_rot);
+            auto cos_1 = cos(glycin_rot);
+            auto sin_1 = sin(glycin_rot);
+            std::cout << glycin_rot << ", " << cos_1 << ", " << sin_1 << std::endl;
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+          {
+            auto glycin_rot_degree = glycin_sprite.getRotation();
+            auto glycin_rot_radials = glycin_rot_degree * M_PI / 180;
+            auto glycin_pos_x = glycin_sprite.getPosition().x;
+            auto glycin_pos_y = glycin_sprite.getPosition().y;
+            glycin_pos_x = glycin_pos_x + cos(-glycin_rot_radials) * 5;
+            glycin_pos_y = glycin_pos_y - sin(-glycin_rot_radials) * 5;
+            glycin_sprite.setPosition(glycin_pos_x, glycin_pos_y);
+          }
+          break;
+        }
         default:
           break;
       }
     }
-    window.clear();
+    window.clear(sf::Color::Cyan);
+    window.draw(glycin_sprite);
     window.display();
   }
 }
