eecf73ddd915f897b6508823779cdb8ee339c55f
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-19 20:42:59 +0100

    work for @CarmenIJsebaart

--- game_test.cpp
+++ game_test.cpp
@@ -136,3 +136,22 @@ BOOST_AUTO_TEST_CASE(check_game_do_action_decellerate_undoes_accelerate)
   );
 
 }
+
+BOOST_AUTO_TEST_CASE(check_game_players_turn_left)
+{
+  game g = create_test_game_1();
+  const auto angle_before = g.get_players()[0].get_rotation();
+  g.do_action(0, action::turn_left);
+  const auto angle_after = g.get_players()[0].get_rotation();
+  BOOST_CHECK(angle_before != angle_after);
+}
+
+BOOST_AUTO_TEST_CASE(check_game_players_turn_right)
+{
+  game g = create_test_game_1();
+  const auto angle_before = g.get_players()[0].get_rotation();
+  g.do_action(0, action::turn_right);
+  const auto angle_after = g.get_players()[0].get_rotation();
+  BOOST_CHECK(angle_before != angle_after);
+}
+
