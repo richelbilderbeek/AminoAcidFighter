20e14dbc07906794ecbeaa611b6cd9191ee8458f
CarmenIJsebaart <cijsebaart@gmail.com>
2017-03-19 21:54:03 +0100

    Tests pass

--- menu.cpp
+++ menu.cpp
@@ -1,19 +1,35 @@
-﻿#include "menu.h"
+﻿#include <stdexcept>
+#include "menu.h"
 
 menu::menu(
   const std::vector<amino_acid>& initial_amino_acids
 )
   : m_amino_acids{initial_amino_acids}
 {
+  if(initial_amino_acids.size() < 2 ||
+     initial_amino_acids.size() > 4)
+  {
+    throw std::invalid_argument("Too many or too little amino_acids");
+  }
 }
 
 void menu::choose_next(const int player_index)
 {
+  if(player_index < 0 ||
+     player_index >= static_cast<int>(m_amino_acids.size()))
+  {
+    throw std::invalid_argument("Invalid player");
+  }
   m_amino_acids[player_index] = get_next(m_amino_acids[player_index]);
 }
 
 void menu::choose_previous(const int player_index)
 {
+  if(player_index < 0 ||
+     player_index >= static_cast<int>(m_amino_acids.size()))
+  {
+    throw std::invalid_argument("Invalid player");
+  }
   m_amino_acids[player_index] = get_prev(m_amino_acids[player_index]);
 }
 
