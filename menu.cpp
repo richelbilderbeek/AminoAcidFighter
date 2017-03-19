9d3fd13181263da8923edae2b2105b4ce3898314
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-19 21:32:42 +0100

    work for @CarmenIJsebaart

--- menu.cpp
+++ menu.cpp
@@ -1,5 +1,16 @@
 ﻿#include "menu.h"
 
+menu create_test_menu_1()
+{
+  const std::vector<amino_acid> initial_amino_acids =
+  {
+    amino_acid::threonine,
+    amino_acid::arginine,
+    amino_acid::phenylalanine
+  };
+  return menu m{initial_amino_acids};
+}
+
 void minus_player(
   int &player_amount)
 {
--- menu_test.cpp
+++ menu_test.cpp
@@ -42,5 +42,50 @@ BOOST_AUTO_TEST_CASE(menu_choose_next)
   BOOST_CHECK(m.get_current_amino_acids()[1] != amino_acid::arginine);
 }
 
+BOOST_AUTO_TEST_CASE(menu_construction_with_too_little)
+{
+  //Cannot create this menu with one amino acid
+  const std::vector<amino_acid> initial_amino_acids =
+  {
+    amino_acid::threonine
+  };
+  BOOST_CHECK_THROW(
+    menu(initial_amino_acids),
+    std::invalid_argument
+  );
+}
+
+BOOST_AUTO_TEST_CASE(menu_construction_with_too_many)
+{
+  //Cannot create this menu with five amino acids
+  const std::vector<amino_acid> initial_amino_acids =
+  {
+    amino_acid::alanine,
+    amino_acid::arginine,
+    amino_acid::asparagine,
+    amino_acid::aspartic_acid,
+    amino_acid::cysteine
+  };
+  BOOST_CHECK_THROW(
+    menu(initial_amino_acids),
+    std::invalid_argument
+  );
+}
+
+BOOST_AUTO_TEST_CASE(menu_choose_for_invalid_players)
+{
+  menu m = create_test_menu_1();
+  BOOST_CHECK_THROW(m.choose_next(-1), std::invalid_argument);
+  BOOST_CHECK_THROW(m.choose_previous(-1), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(menu_choose_for_absent_players)
+{
+  menu m = create_test_menu_1();
+  const int n_players = m.get_current_amino_acids().size();
+  const int absent_player_index = n_players;
+  BOOST_CHECK_THROW(m.choose_next(absent_player_index), std::invalid_argument);
+  BOOST_CHECK_THROW(m.choose_previous(absent_player_index), std::invalid_argument);
+}
 
 
