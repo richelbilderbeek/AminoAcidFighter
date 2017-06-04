3ba7bc569503384a4aa24e1bfb3c7e281477c8b6
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-04 16:46:30 +0200

    Moved STL-only code from program_sfml to program

--- AminoAcidFighter.pri
+++ AminoAcidFighter.pri
@@ -1,30 +1,32 @@
 SOURCES += \
-    action.cpp \
-    amino_acid.cpp \
-    bullet.cpp \
-    game.cpp \
-    helper.cpp \
-    player.cpp \
-    program_state.cpp \
-    game_state.cpp \
-    power.cpp \
+    $$PWD/action.cpp \
+    $$PWD/amino_acid.cpp \
+    $$PWD/bullet.cpp \
+    $$PWD/game.cpp \
+    $$PWD/helper.cpp \
+    $$PWD/player.cpp \
+    $$PWD/program.cpp \
+    $$PWD/program_state.cpp \
+    $$PWD/game_state.cpp \
+    $$PWD/power.cpp \
     $$PWD/choose_n_players_menu.cpp \
     $$PWD/choose_amino_acids_menu.cpp \
     $$PWD/winner_screen.cpp
 
 HEADERS += \
-    action.h \
-    amino_acid.h \
-    bullet.h \
-    helper.h \
-    game.h \
-    player.h \
-    program_state.h \
-    game_state.h \
-    power.h \
+    $$PWD/action.h \
+    $$PWD/amino_acid.h \
+    $$PWD/bullet.h \
+    $$PWD/helper.h \
+    $$PWD/game.h \
+    $$PWD/player.h \
+    $$PWD/program.h \
+    $$PWD/program_state.h \
+    $$PWD/game_state.h \
+    $$PWD/power.h \
     $$PWD/choose_n_players_menu.h \
     $$PWD/choose_amino_acids_menu.h \
     $$PWD/winner_screen.h
 
 RESOURCES += \
-    AminoAcidFighter.qrc
+    $$PWD/AminoAcidFighter.qrc
--- program_sfml.h
+++ program_sfml.h
@@ -53,36 +53,5 @@ private:
   void run_winner_screen();
 };
 
-///Create amino acids depending on the command-line arguments.
-///This differs between a normal and profile run
-std::vector<amino_acid> create_amino_acids(
-  const std::vector<std::string>& args) noexcept;
-
-///Create the first default amino acids for the 'select amino acids' menu,
-///and also to let the 'choose number of amino acids' have a valid number
-std::vector<amino_acid> create_first_amino_acids() noexcept;
-
-///Create the amino acids used in profiling
-std::vector<amino_acid> create_profiling_amino_acids() noexcept;
-
-///Derive the first program_state from the command-line arguments. This
-///differs between (1) a normal run, (2) a profile run, (3) a request for
-///the help pages
-program_state create_state(const std::vector<std::string>& args) noexcept;
-
-///Is there a command-line argument (as passed from the 'main' function),
-///that indicates that the user wants no music?
-bool do_play_music(const std::vector<std::string>& args) noexcept;
-
-///Is there a command-line argument (as passed from the 'main' function),
-///that indicates that the user wants to show the help pages?
-bool is_help(const std::vector<std::string>& args) noexcept;
-
-///Is there a command-line argument (as passed from the 'main' function),
-///that indicates that the user wants a profile run?
-bool is_profile_run(const std::vector<std::string>& args) noexcept;
-
-///The show the help screen in a terminal
-void show_help();
 
 #endif // PROGRAM_SFML_H
