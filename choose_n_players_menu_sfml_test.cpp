ba170ae1c59ee8ebf3083fb783a3436212f92371
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-04 17:50:03 +0200

    Test choose_n_players_menu_sfml

--- choose_n_players_menu_sfml_test.cpp
+++ choose_n_players_menu_sfml_test.cpp
@@ -4,6 +4,15 @@
 
 BOOST_AUTO_TEST_CASE(choose_n_players_menu_sfml_construction)
 {
-
+  sf::RenderWindow w;
+  const bool do_play_music{false};
+  const int n_players{3};
+  choose_n_players_menu_sfml m(
+    w,
+    do_play_music,
+    n_players
+  );
+  BOOST_CHECK_EQUAL(m.do_play_music(), do_play_music);
+  BOOST_CHECK_EQUAL(m.get_n_players(), n_players);
 }
 
